99d77a0db6e40eb825feb7e347a94cc92f42de1e
doguz2509 <vWMA25Y6Vmc4UJSW4vUB>
2022-05-02 16:16:05 +0300

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cloud_lightweight_manager',
+    version='0.0.1',
+    packages=['cloud_lightweight_manager'],
+    url='',
+    license='MIT',
+    author='Dmitry Oguz',
+    author_email='doguz2509@gmail.com',
+    description='Cloud Light weight manager'
+)
